302dd4d7da3bc9e3c66508493a2093cb89455a75
Krzysztof Aleksander Andres <47752738+krzysztof176@users.noreply.github.com>
2024-12-14 14:54:47 -0500

    Update File Organizer.py

--- File Organizer.py
+++ File Organizer.py
@@ -20,16 +20,16 @@ filesLocation = r"{}".format(input("Where are the files located?\n"))
 # 1: Make a copy of the original files and place them to the organize folders.
 keepOriginalFiles = bool(int(input("Do you want to keep the original files? 1 for yes, 0 for no\n")))
 
+# 0: Change time returns the creation time of a file or directory at the specified path.
+#    I should never use 0 since I just moved all the files into the nas.
+#    The change time will show as of today not when the file was created.
+# 1: Modification time returns last modification time.
+#    In my case it will be the true created time of the file since I do not modify pictures or videos.
+useModificationTime = bool(int(input("Do you want to use modification time(1) or change time(0)?\n")))
+
 # Go over all of the files
 for fileName in os.listdir(filesLocation):
   fileLocation = "{}\\{}".format(filesLocation, fileName)
-
-  # 0: Change time returns the creation time of a file or directory at the specified path.
-  #    I should never use 0 since I just moved all the files into the nas.
-  #    The change time will show as of today not when the file was created.
-  # 1: Modification time returns last modification time.
-  #    In my case it will be the true created time of the file since I do not modify pictures or videos.
-  useModificationTime = bool(int(input("Do you want to use modification time(1) or change time(0)?\n")))
   
   createdTime = time.ctime(os.path.getmtime(fileLocation) if useModificationTime else os.path.getmtime(fileLocation))
   createdDateTime = datetime.datetime.strptime(createdTime, "%a %b %d %H:%M:%S %Y")
